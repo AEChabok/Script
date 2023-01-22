2215e5923277e269b54d5b2f29e454bab33e72de
Amir Ezazi <51764116+AEChabok@users.noreply.github.com>
2023-01-22 10:38:23 +0330

    Create MySqlBackup.sh
    Bash Script To Backup All Database In Mysql Server Hosts.

---
+++ MySqlBackup.sh
@@ -0,0 +1,48 @@
+#! /bin/bash
+
+# Backup MySql Server
+# by Amir Ezazi
+
+# Default Username & Password Of Mysql Application
+USERNAME=root
+PASSWORD=aec@2647@39294
+
+# Default Directory Mysql Application Path
+MYSQL="/usr/bin/mysql"
+MYDUMP="/usr/bin/mysqldump"
+COMPRESSOR="/usr/bin/zstd"
+
+# Default Directory To Save Backups In, Must Be rwx By Mysql User
+BACKUPTIME=30
+BACKUPHOST=127.0.0.1
+BACKUPPATH="/home/ae/backup"
+BACKUPDATE=$(date "+%Y-%m-%d")
+BACKUPDIR="$BACKUPPATH/$BACKUPDATE"
+
+# Create Backup Directory By Date Time
+mkdir -p $BACKUPDIR
+cd $BACKUPDIR
+
+# Get List Of Databases In System, Remove The Table Header
+DATABASES=$(echo "SHOW DATABASES" | $MYSQL -h $BACKUPHOST -u $USERNAME -p$PASSWORD | egrep -v 'Database')
+
+# Now Loop Through Each Individual Database And Backup The Database Separately
+for DATABASE in $DATABASES; do
+    #Create Directory For Each Database
+    DIRECTORY=$(echo $DATABASE | sed 's/.*/\u&/') 
+    mkdir -p $DIRECTORY
+
+    # Export Data From Mysql Databases To Plain Text
+    $MYDUMP --opt --user=${USERNAME} --password=${PASSWORD} ${DATABASE} > ${DIRECTORY}"/"${DATABASE}".sql"
+
+    # Compress Files
+    $COMPRESSOR -z --rm ${DIRECTORY}"/"${DATABASE}".sql"
+done
+
+# Delete Backup Files Older Than ${BACKUPTIME} Days
+OLDBACKUP=$(find $BACKUPPATH -type d -mtime +${BACKUPTIME})
+
+if [ -n "$OLDBACKUP" ] ; then
+        echo Deleting Old Backup Files: $OLDBACKUP
+        echo $OLDBACKUP | xargs rm -rfv
+fi
